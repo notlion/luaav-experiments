d071e58a52854446210dd0fb2d9ee874095a00f0
Ryan Alexander <ryan@Bom.local>
2014-01-05 20:59:55 -0800

    Add support for non-square monomes

--- seq.lua
+++ seq.lua
@@ -1,40 +1,41 @@
 local Seq = {}
 Seq.__index = Seq
 
-local function createGrid(dim)
+local function createGrid(nx, ny)
   local grid = {}
-  for i = 1, 8 * 8 do grid[i] = 0 end
+  for i = 1, nx * ny do grid[i] = 0 end
   return grid
 end
 
-function Seq:new(dim)
+function Seq:new(nx, ny)
   seq = {
-    grid = createGrid(dim),
-    dim = dim,
+    grid = createGrid(nx, ny),
+    nx = nx,
+    ny = ny,
     position = 0
   }
   return setmetatable(seq, self)
 end
 
 function Seq:get(x, y)
-  return self.grid[y * self.dim + x + 1]
+  return self.grid[y * self.nx + x + 1]
 end
 
 function Seq:set(x, y, value)
-  self.grid[y * self.dim + x + 1] = value
+  self.grid[y * self.nx + x + 1] = value
 end
 
 function Seq:play(callback)
-  local dim, scale, position = self.dim, self.scale, self.position
+  local scale, position = self.scale, self.position
 
-  for x = 0, dim - 1 do
-    local v = self:get(x, position)
+  for y = 0, self.ny - 1 do
+    local v = self:get(position, y)
     if v > 0 then
-      callback(x, y)
+      callback(position, y)
     end
   end
 
-  self.position = (position + 1) % dim
+  self.position = (position + 1) % self.nx
 end
 
 return Seq
--- step-seq.lua
+++ step-seq.lua
@@ -1,20 +1,22 @@
 local osc = require('osc')
 local oscIn = osc.Recv(8000)
-local oscOut = osc.Send('localhost', 15073)
+local oscOut = osc.Send('localhost', 10381)--15073)
 local util = require('audio.util')
 local Def = require('audio.Def')
 Def.globalize()
 
-local seq = require('seq'):new(8)
+local seq = require('seq'):new(16, 8)
 
 local function getOscMessages()
   for msg in oscIn:recv() do
     if msg.addr == '/monome/grid/key' then
+      print(unpack(msg))
       local x, y, v = unpack(msg)
+      local rx = seq.nx - x - 1 -- Reverse X
       if v == 1 then
-        local pv = seq:get(x, y)
+        local pv = seq:get(rx, y)
         if pv > 0 then v = 0 else v = 1 end
-        seq:set(x, y, v)
+        seq:set(rx, y, v)
         oscOut:send('/monome/grid/led/set', x, y, v)
       end
     end
@@ -24,14 +26,16 @@ end
 local synth = Def{
   cf    = 300,
   mf    = 550,
-  index = 5,
-  dur   = 0.5,
-
-  Env{ dur = P"dur" } *
-
-  SinOsc{
-    freq = P"cf" + SinOsc{ freq = P"mf" } * (P"mf" * P"index")
-  } * 0.25
+  index = 8.1,
+  dur   = 0.25,
+  pan   = 0,
+  Pan2{
+    Env{ dur = P"dur" } *
+    SinOsc{
+      freq = P"cf" + SinOsc{ freq = P"mf" } * (P"mf" * P"index")
+    } * 0.25,
+    P"pan"
+  }
 }
 
 local Mixer = Def{
@@ -52,19 +56,22 @@ oscOut:send('/monome/grid/led/all', 0)
 
 local scale = { 0, 2, 4, 7, 9 } -- Pentatonic Major
 
+local i = 0
 repeat
   getOscMessages()
   seq:play(function(x, y)
-    local octave = math.floor(x / #scale) - 1
-    local note = scale[x % #scale + 1]
+    local octave = math.floor(y / #scale) - 1
+    local note = scale[y % #scale + 1]
     local freq = util.mtof(69 + octave * 12 + note)
     synth{
       cf = freq,
       mf = freq * 2.01,
-      amp = (1 / seq.dim) * 0.5,
-      pan = seq.position / (seq.dim - 1) * 2 - 1,
+      index = 2 + math.floor(i % 30),
+      amp = (1 / seq.ny) * 0.5,
+      -- pan = seq.position / (seq.dim - 1) * 2 - 1,
       out = bus
     }
   end)
+  i = i + 1
   wait(1/8)
 until false
